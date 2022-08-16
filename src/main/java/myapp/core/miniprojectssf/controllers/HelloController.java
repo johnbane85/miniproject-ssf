0c9313efbe63ddae99c1b99a1db2ebbe24cb3252
johnbane85 <junyu.siew@gmail.com>
2022-08-16 15:11:53 +0800

    new commit

--- src/main/java/myapp/core/miniprojectssf/controllers/HelloController.java
+++
@@ -1,8 +0,0 @@
-package myapp.core.miniprojectssf.controllers;
-
-import org.springframework.stereotype.Controller;
-
-@Controller
-public class HelloController {
-
-}
---
+++ src/main/java/myapp/core/miniprojectssf/controllers/IndexResource.java
@@ -0,0 +1,25 @@
+package myapp.core.miniprojectssf.controllers;
+
+import java.util.Calendar;
+import java.util.Date;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping(path = { "/", "/indexresource", "indexresource.html" })
+public class IndexResource {
+
+  @GetMapping(produces = { "text/html" })
+  public String index(Model model) {
+    model.addAttribute("currTime", (new Date()).toString());
+
+    Calendar cal = Calendar.getInstance();
+    model.addAttribute("currHour", cal.get(Calendar.HOUR_OF_DAY));
+
+    return "indexresource";
+  }
+
+}
