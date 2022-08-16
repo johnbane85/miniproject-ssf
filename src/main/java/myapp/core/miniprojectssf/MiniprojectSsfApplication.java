df1cd82048f5de2cbe668610aafe06b9f0c0cb3b
johnbane85 <junyu.siew@gmail.com>
2022-08-16 14:02:31 +0800

    1st commit

---
+++ src/main/java/myapp/core/miniprojectssf/MiniprojectSsfApplication.java
@@ -0,0 +1,13 @@
+package myapp.core.miniprojectssf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniprojectSsfApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniprojectSsfApplication.class, args);
+	}
+
+}
---
+++ src/test/java/myapp/core/miniprojectssf/MiniprojectSsfApplicationTests.java
@@ -0,0 +1,13 @@
+package myapp.core.miniprojectssf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniprojectSsfApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
